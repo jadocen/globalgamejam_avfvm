0c22ba045088a62865b6f1bcee6291393bb2033f
jadocen <angelic.rabano@benilde.edu.ph>
2024-01-26 15:23:47 +0800

    Update Health ulet

---
+++ AVFVM/Assets/Scripts/BossBase.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossBase : MonoBehaviour
+{
+    public float _health;
+
+    private float _moveSpeed;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
