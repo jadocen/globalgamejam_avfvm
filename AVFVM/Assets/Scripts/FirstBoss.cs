e0cfa9763c292b9a8634a04c356c824c8fc2472a
sese-moises <moisesferdinand.sese@benilde.edu.ph>
2024-01-27 08:34:17 +0800

    Renamed scenes

--- AVFVM/Assets/Scripts/FirstBoss.cs
+++ AVFVM/Assets/Scripts/FirstBoss.cs
@@ -12,9 +12,22 @@ public class FirstBoss : BossBase
 
     public override IEnumerator Attack1()
     {
-        float fireRate = 1f;
+        float fireRate = 0.8f;
         GameObject projectile = Instantiate(_projectileGO[_choice], transform.position, _aimDirection.transform.rotation);
         projectile.GetComponent<Projectile>().Initialize(_projectileSpeed[_choice], 3f);
+        yield return new WaitForSeconds(fireRate);
+        _canShoot = true;
+    }
+
+    public override IEnumerator Attack2()
+    {
+        float fireRate = 1.3f;
+        for (int i = -30; i <= 30; i += 15)
+        {
+            Vector3 angle = new Vector3(_aimDirection.transform.eulerAngles.x, _aimDirection.transform.eulerAngles.y, _aimDirection.transform.eulerAngles.z - i);
+            GameObject projectile = Instantiate(_projectileGO[_choice], transform.position, Quaternion.Euler(angle));
+            projectile.GetComponent<Projectile>().Initialize(_projectileSpeed[_choice], 3f);
+        }
         yield return new WaitForSeconds(fireRate);
         _canShoot = true;
     }
