2c5e5627b66bc70dcb3144073dc47a4523ae59b6
jadocen <angelic.rabano@benilde.edu.ph>
2024-01-28 07:04:51 +0800

    In-Game UI

--- AVFVM/Assets/Scripts/FollowPlayer.cs
+++ AVFVM/Assets/Scripts/FollowPlayer.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FollowPlayer : MonoBehaviour
 {
     public Transform player;
+    public Image _health;
 
     private void Update()
     {
+        _health.fillAmount = player.GetComponent<HealthComponent>()._health / 10;
+
         //Camera position follows player + offset
         if (player != null)
         {
